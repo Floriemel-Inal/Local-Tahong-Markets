240087f9eec05fdc0e2dcf42d5588ac5c22cf183
Oscar T. Maribojoc III <oscartmaribojociii@gmail.com>
2025-06-13 13:45:34 +0800

    Full rebuild for GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "83f02a28a663492fc1c7e62f79cd7628
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "36404abc8124a8d3b4cb02ccc8acf286",
+"flutter_bootstrap.js": "3d7df53574de952862607ab13bfb27cb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
